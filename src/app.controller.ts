717131b0b3746aa79b7e1fb8b9cf8037fd2f8a60
Umut Kızıloğlu <umutkiziloglu00@gmail.com>
2023-02-12 18:46:23 +0000

    controller updated

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,33 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AstrologyDataRequest } from 'dto/astrology.dto';
+import {
+  AstrologyDateDataResponse,
+  AstrologyFeatureDataResponse,
+} from 'interfaces/astrology.interface';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(): object {
+    return {
+      message: 'Hello World!',
+    };
+  }
+
+  @Get('/astroloji')
+  async getAstrology(
+    @Query() { sign, type, date, feature }: AstrologyDataRequest,
+  ): Promise<AstrologyDateDataResponse | AstrologyFeatureDataResponse> {
+    if (type === 'date') {
+      return await this.appService.getHoroscopeByDate({ sign, date });
+    } else if (type === 'feature') {
+      return await this.appService.getHoroscopeByFeature({
+        sign,
+        feature,
+      });
+    }
   }
 }
