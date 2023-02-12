335b1f528f4f34bfc170a352008f8ff6928fa413
Umut Kızıloğlu <umutkiziloglu00@gmail.com>
2023-02-12 18:46:41 +0000

    main updated

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
