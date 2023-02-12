e5f011a988ed4084427d25659db8ac21ca11d219
Umut Kızıloğlu <umutkiziloglu00@gmail.com>
2023-02-12 18:46:09 +0000

    cast helper added

---
+++ helpers/cast.helper.ts
@@ -0,0 +1,78 @@
+interface ToNumberOptions {
+  default?: number;
+  min?: number;
+  max?: number;
+}
+
+export function toLowerCase(value: string): string {
+  return value.toLowerCase();
+}
+
+export function trim(value: string): string {
+  return value.trim();
+}
+
+export function toDate(value: string): Date {
+  return new Date(value);
+}
+
+export function toBoolean(value: string): boolean {
+  value = value.toLowerCase();
+
+  return value === 'true' || value === '1' ? true : false;
+}
+
+export function toNumber(value: string, opts: ToNumberOptions = {}): number {
+  let newValue: number = Number.parseInt(value || String(opts.default), 10);
+
+  if (Number.isNaN(newValue)) {
+    newValue = opts.default;
+  }
+
+  if (opts.min) {
+    if (newValue < opts.min) {
+      newValue = opts.min;
+    }
+
+    if (newValue > opts.max) {
+      newValue = opts.max;
+    }
+  }
+
+  return newValue;
+}
+
+export function turkishToLower(value: string): string {
+  return value
+    .toLowerCase()
+    .replace(/ğ/g, 'g')
+    .replace(/ü/g, 'u')
+    .replace(/ş/g, 's')
+    .replace(/ı/g, 'i')
+    .replace(/ö/g, 'o')
+    .replace(/ç/g, 'c');
+}
+
+export function turkishToUpper(value: string): string {
+  return value
+    .toUpperCase()
+    .replace(/Ğ/g, 'G')
+    .replace(/Ü/g, 'U')
+    .replace(/Ş/g, 'S')
+    .replace(/I/g, 'I')
+    .replace(/Ö/g, 'O')
+    .replace(/Ç/g, 'C');
+}
+
+export function slugify(value: string): string {
+  return value
+    .toLowerCase()
+    .replace(/ğ/g, 'g')
+    .replace(/ü/g, 'u')
+    .replace(/ş/g, 's')
+    .replace(/ı/g, 'i')
+    .replace(/ö/g, 'o')
+    .replace(/ç/g, 'c')
+    .replace(/[^a-z0-9]+/g, '-')
+    .replace(/^-+|-+$/g, '');
+}
